b36287e3a4393eaf012cd5049904c4fb9b0e0780
Gabriel91017 <47323206+Gabriel91017@users.noreply.github.com>
2021-07-20 19:10:39 -0500

    Se anexa 404

---
+++ 404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.min.css" />
+    <link rel="stylesheet" href="css/queries.min.css" />
+    <title>Ooops... Te has perdido | HULA Wings&reg;</title>
+</head>
+<body>
+    <div class="bck404">
+        <img id="logo-404" src="img/logo_hula.svg" alt="HULA Wings">
+        <h1>Error<br><span>404</span></h1>
+        <img class="cerveza404" src="bebidas/cerveza.png" alt="">
+        <a href="/"><button id="btn-404" class="drinks-btn">Volver a Inicio</button></a>
+    </div>
+</body>
+</html>
