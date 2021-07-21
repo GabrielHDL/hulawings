49cf62701c6e2caece41eab39f2f84ed869f77d3
Gabriel91017 <47323206+Gabriel91017@users.noreply.github.com>
2021-07-20 19:14:28 -0500

    404 update

--- 404.php
+++ 404.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <!-- Google Tag Manager -->
+    <script>
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MW6SMNN');
+    </script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.min.css" />
     <link rel="stylesheet" href="css/queries.min.css" />
+    <link rel="shortcut icon" href="assets/favicon.png" />
     <title>Ooops... Te has perdido | HULA Wings&reg;</title>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MW6SMNN"
+        height="0" width="0" style="display:none;visibility:hidden">
+      </iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div class="bck404">
         <img id="logo-404" src="img/logo_hula.svg" alt="HULA Wings">
         <h1>Error<br><span>404</span></h1>
