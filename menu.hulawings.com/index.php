5e1acac43cb88a44b08b5f8be55816051a4c85b9
Gabriel91017 <47323206+Gabriel91017@users.noreply.github.com>
2021-08-20 11:33:05 -0500

    Update Major release

---
+++ menu.hulawings.com/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="shortcut icon" href="https://hulawings.com/assets/favicon.png" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <title>Menú | HULA Wings&reg;</title>
+</head>
+<body>
+    <div class="menu-heading">
+        <div class="card">
+            <h1>Menú</h1>
+        </div>
+    </div>
+    <div class="menu-content">
+        <a href="https://hulawings.com/drinks/"><button class="btn-menu" id="drinks">Drinks</button></a>
+        <a href="https://hulawings.com/snacks/"><button class="btn-menu" id="snacks">Snacks</button></a>
+    </div>
+</body>
+</html>
